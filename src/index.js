2d1a1ec918511324346f9c68a46120b77ea75f47
Deepak Choudhary <choudharydeepak938@gmail.com>
2020-11-18 23:46:32 +0530

    trial 11

--- src/index.js
+++ src/index.js
@@ -52,7 +52,7 @@ app.post("/api/student", (req,res)=>{
 
     //res.setHeader(['{"content-type":"application/x-www-form-urlencoded"}']);
     let id = student.id; 
-    res.send({id : id});
+    res.json({id : id});
 })
 
 app.put("/api/student/:id", (req,res)=>{
